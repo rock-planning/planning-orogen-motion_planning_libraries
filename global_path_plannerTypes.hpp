fc36b3e7922a35446583de3b56c9b600fb705c7a
Stefan Haase <stefan.haase@dfki.de>
2014-01-30 15:54:46 +0100

    initial commit by rock-create-orogen

---
+++ global_path_plannerTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef global_path_planner_TYPES_HPP
+#define global_path_planner_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace global_path_planner {
+}
+
+#endif
+
