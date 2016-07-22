eeaa8ec867cefb9e6a37c6944228230d0685650c
Spacebot Groundstation Big <ric-spacebot@dfki.de>
2016-07-22 16:52:06 +0200

    Just an adapted test script.

--- scripts/test_planning.rb
+++ scripts/test_planning.rb
@@ -13,10 +13,10 @@ ENV['LANG'] = 'C'
 ENV['LC_NUMERIC'] = 'C'
 
 Orocos.run  'motion_planning_libraries::Task' => 'planner',
-            'motion_planning_libraries::Test' => 'test',
-            "valgrind" => false,
-            'output' => nil, 
-            "wait" => 1000 do
+            'motion_planning_libraries::Test' => 'test' do
+            #"valgrind" => false,
+            #'output' => nil, 
+            #"wait" => 1000 do
           
     planner = TaskContext::get 'planner'
     planner.traversability_map_id = "trav" 
@@ -24,8 +24,8 @@ Orocos.run  'motion_planning_libraries::Task' => 'planner',
     planner.only_provide_optimal_trajectories = true
     
     planner.config do |p|
-        p.mPlanningLibType = :LIB_OMPL
-        p.mEnvType = :ENV_SHERPA
+        p.mPlanningLibType = :LIB_SBPL
+        p.mEnvType = :ENV_XYTHETA
         p.mPlanner = :UNDEFINED_PLANNER
         p.mFootprintLengthMinMax.first = 1.0
         p.mFootprintLengthMinMax.second = 1.0
@@ -53,10 +53,11 @@ Orocos.run  'motion_planning_libraries::Task' => 'planner',
         p.mMobility.mTurningSpeed = 0.15
         p.mMobility.mMultiplierForward = 1
         p.mMobility.mMultiplierBackward = 2
-        p.mMobility.mMultiplierLateral = 0
+        p.mMobility.mMultiplierLateral = 3
         p.mMobility.mMultiplierForwardTurn = 2
         p.mMobility.mMultiplierBackwardTurn = 3
         p.mMobility.mMultiplierPointTurn = 4
+        p.mMobility.mMultiplierLateralCurve = 4
         p.mMobility.mMinTurningRadius = 1.0
         
         # SBPL specific configuration
@@ -73,7 +74,7 @@ Orocos.run  'motion_planning_libraries::Task' => 'planner',
 
     test = TaskContext::get 'test'
     test.traversability_map_id = 'trav'
-    test.traversability_map_type = 'SMALL_OPENING'
+    test.traversability_map_type = 'RANDOM_CIRCLES'
     test.traversability_map_width_m = 120
     test.traversability_map_height_m = 10
     test.traversability_map_scalex =  0.1   
@@ -97,7 +98,7 @@ Orocos.run  'motion_planning_libraries::Task' => 'planner',
             test.trigger
         end
     #end
-=begin
+#=begin
     Vizkit.display planner
     Vizkit.display test.port("traversability_map")
     #Vizkit.display test.port("start_pose_samples"), :widget => Vizkit.default_loader.RigidBodyStateVisualization
@@ -112,7 +113,7 @@ Orocos.run  'motion_planning_libraries::Task' => 'planner',
     Vizkit.display planner.sbpl_mprims_debug
  
     Vizkit.exec
-=end
+#=end
     Readline::readline("Hit ENTER to stop")    
 end
   
